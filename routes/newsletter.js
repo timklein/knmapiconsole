2d0daee2d8acd9a118e63e324e599e2f11e8688c
Tim Klein <timklein@users.noreply.github.com>
2018-09-26 17:59:51 -0600

    Build connection dropdown for newsletter configurations

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,12 +1,14 @@
 //Routes for Newletter COnfiguration
 const express = require('express');
-// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
+const connections = require('../controllers/infusionsoftTokenController');
 
 //GET Console page for CallRail Settings
-router.get('/', (req, res) => {
+router.get('/', ensureLoggedIn, connections.buildList, (req, res) => {
     res.render('newsletter', {
-        user : req.user
+        user : req.user,
+        apps : req.apps
     });
 });
 
