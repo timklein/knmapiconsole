2b3c218225c2af2bd3cbf40772f6ef33805e32c5
Tim Klein <timklein@users.noreply.github.com>
2019-07-16 14:57:25 -0600

    Config to use local DEV database

--- models/db.js
+++ models/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_CONNECT_STRING, { useNewUrlParser : true});
+if (process.env.NODE_ENV == 'DEV') {
+	mongoose.connect(process.env.MONGODB_DEV_CONNECT_STRING, { useNewUrlParser : true});
+} else {
+	mongoose.connect(process.env.MONGODB_CONNECT_STRING, { useNewUrlParser : true});
+}
 
 // Use native promises
 mongoose.Promise = global.Promise;
