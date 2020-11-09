d85f52464ecf6d7dac80b1ec8aa9dbedddddacf3
Tim Klein <timklein@users.noreply.github.com>
2020-11-09 16:23:26 -0700

    Move Delay

--- controllers/keapGoalController.js
+++ controllers/keapGoalController.js
@@ -16,29 +16,32 @@ const controller = {
 	getToken : function (req, res, next) {
 
 		if (req.params.integration && req.params.callName) {
-			Token.findOne({ app_code : req.params.integration }, 'access_token', function (err, token) {
-				if (err) {
-	                msg.text = 'Access Token Retrieval Error: ' +  req.params.integration;
-					msg.html = '<p><strong>Access Token Retrieval From Database Generated an Error</strong></p><p>Database retrieval of access token for ' + req.params.integration + ' generated an error. Please check system logs for error details.</p>';
-	                sgMail.send(msg);
-	                
-	                console.log(err);
-					res.sendStatus(200);
-				}
-				else if (token) {
-					req.body.access_token = token.access_token;
-					next();
-				}
-				else {
-					msg.text = 'Access Token Retrieval Error: ' +  req.params.integration;
-					msg.html = '<p><strong>Access Token Retrieval From Database Resulted in an Unknown Error</strong></p><p>Database retrieval of access token for ' + req.params.integration + ' resulted in an unknown error.</p>';
-	                sgMail.send(msg);
+			
+			setTimeout( () => {
+				Token.findOne({ app_code : req.params.integration }, 'access_token', function (err, token) {
+					if (err) {
+						msg.text = 'Access Token Retrieval Error: ' +  req.params.integration;
+						msg.html = '<p><strong>Access Token Retrieval From Database Generated an Error</strong></p><p>Database retrieval of access token for ' + req.params.integration + ' generated an error. Please check system logs for error details.</p>';
+						sgMail.send(msg);
+						
+						console.log(err);
+						res.sendStatus(200);
+					}
+					else if (token) {
+						req.body.access_token = token.access_token;
+						next();
+					}
+					else {
+						msg.text = 'Access Token Retrieval Error: ' +  req.params.integration;
+						msg.html = '<p><strong>Access Token Retrieval From Database Resulted in an Unknown Error</strong></p><p>Database retrieval of access token for ' + req.params.integration + ' resulted in an unknown error.</p>';
+						sgMail.send(msg);
 
-					console.log('Unknown Token Retrieval Error');
-					console.log(req.params.integration);
-					res.sendStatus(200);
-				}
-			});			
+						console.log('Unknown Token Retrieval Error');
+						console.log(req.params.integration);
+						res.sendStatus(200);
+					}
+				});
+			}, 5000);		
 		}
 		else {
             msg.text = 'Incoming Webhook Error: ' +  req.params.integration + ' is missing required data.';
@@ -67,28 +70,26 @@ const controller = {
 				json : true
 			};
 	
-			setTimeout(() => { 
-				rp(goalOptions)
-				.then( function (response) {
+			rp(goalOptions)
+			.then( function (response) {
+				
+				if (response) {
+	
+					let msg = JSON.stringify(response);
 					
-					if (response) {
-		
-						let msg = JSON.stringify(response);
-						
-						console.log('Goal Completion Response for Contact ID #' + req.body.object_keys[0].id + ': ' + msg);
-						res.sendStatus(200);
-					}
-				})
-				.catch( function (err) {
-					msg.text = 'API Error on Goal Completion';
-					msg.html = '<p><strong>API Error on Goal Completion</strong></p><p>API Call to ' + req.params.integration + ' generated an error for contact ID ' + JSON.stringify(req.body.object_keys[0].id) + ' when trying to complete goal ' + req.params.callName + '. Please review logs for error details.</p>';
-					sgMail.send(msg);
-
-					console.log(req.params.callName + ' goal completion failed in ' + req.params.integration + ' for contact ' + req.body.object_keys[0].id);
-					console.log(err);
+					console.log('Goal Completion Response for Contact ID #' + req.body.object_keys[0].id + ': ' + msg);
 					res.sendStatus(200);
-				});
-			}, 5000);
+				}
+			})
+			.catch( function (err) {
+                msg.text = 'API Error on Goal Completion';
+			    msg.html = '<p><strong>API Error on Goal Completion</strong></p><p>API Call to ' + req.params.integration + ' generated an error for contact ID ' + JSON.stringify(req.body.object_keys[0].id) + ' when trying to complete goal ' + req.params.callName + '. Please review logs for error details.</p>';
+                sgMail.send(msg);
+
+				console.log(req.params.callName + ' goal completion failed in ' + req.params.integration + ' for contact ' + req.body.object_keys[0].id);
+				console.log(err);
+				res.sendStatus(200);
+			});
 		}
 		else {
             console.log('Goal Completion Failed: No Contact ID Available');
